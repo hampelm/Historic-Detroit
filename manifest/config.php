1b0238adc7848b35df0c2768b109bc9a5b6f7704
Matt Hampel <matth@matt-hampel.gateway.2wire.net>
2011-06-29 21:16:37 -0400

    Initial commit

---
+++ manifest/config.php
@@ -0,0 +1,96 @@
+<?php
+
+	$settings = array(
+
+
+		###### ADMIN ######
+		'admin' => array(
+			'max_upload_size' => '5242880',
+		),
+		########
+
+
+		###### SYMPHONY ######
+		'symphony' => array(
+			'pagination_maximum_rows' => '17',
+			'allow_page_subscription' => '1',
+			'lang' => 'en',
+			'version' => '2.2.1',
+			'pages_table_nest_children' => 'yes',
+			'strict_error_handling' => 'yes',
+			'cookie_prefix' => 'sym-',
+		),
+		########
+
+
+		###### LOG ######
+		'log' => array(
+			'archive' => '1',
+			'maxsize' => '102400',
+		),
+		########
+
+
+		###### GENERAL ######
+		'general' => array(
+			'sitename' => 'Buildings of Detroit',
+			'useragent' => 'Symphony/2.2.1',
+		),
+		########
+
+
+		###### IMAGE ######
+		'image' => array(
+			'cache' => '1',
+			'quality' => '90',
+		),
+		########
+
+
+		###### DATABASE ######
+		'database' => array(
+			'character_set' => 'utf8',
+			'character_encoding' => 'utf8',
+			'runtime_character_set_alter' => '1',
+			'query_caching' => 'default',
+			'host' => '127.0.0.1',
+			'port' => '3306',
+			'user' => 'root',
+			'password' => NULL,
+			'db' => 'buildings',
+			'tbl_prefix' => 'sym_',
+		),
+		########
+
+
+		###### PUBLIC ######
+		'public' => array(
+			'display_event_xml_in_source' => 'yes',
+		),
+		########
+
+
+		###### REGION ######
+		'region' => array(
+			'time_format' => 'g:i a',
+			'date_format' => 'd F Y',
+			'datetime_separator' => ' ',
+			'timezone' => 'America/Detroit',
+		),
+		########
+
+
+		###### FILE ######
+		'file' => array(
+			'write_mode' => '0775',
+		),
+		########
+
+
+		###### DIRECTORY ######
+		'directory' => array(
+			'write_mode' => '0775',
+		),
+		########
+	);
+
